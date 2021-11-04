b12818800f69ee3fbd820489f6f4c22d9e694f60
BjornMunthe <Bjomu1011@skola.goteborg.se>
2021-11-04 10:44:27 +0100

    the begining

---
+++ toiletprojectbackend/src/test/java/com/jasb/toiletproject/ToiletprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasb.toiletproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiletprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
