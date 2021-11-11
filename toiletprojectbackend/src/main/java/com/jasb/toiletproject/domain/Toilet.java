8d322fbb6324b78255615e6db3fd4511b6977627
jensbankel <jenban1204@skola.goteborg.se>
2021-11-11 14:03:10 +0100

    working db....maybe

--- toiletprojectbackend/src/main/java/com/jasb/toiletproject/domain/Toilet.java
+++ toiletprojectbackend/src/main/java/com/jasb/toiletproject/domain/Toilet.java
@@ -1,9 +1,16 @@
 package com.jasb.toiletproject.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Toilet {
 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     private long id;
     private double longitude;
     private double latitude;
