4ee85ced1d74f09a0c7ccf2bda75054e60aa8929
BjornMunthe <Bjomu1011@skola.goteborg.se>
2021-11-16 20:30:06 +0100

    some experiments with security

---
+++ toiletprojectbackend/src/main/java/com/jasb/toiletproject/jwt/JwtConfig.java
@@ -0,0 +1,52 @@
+package com.jasb.toiletproject.jwt;
+
+import com.google.common.net.HttpHeaders;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Configuration
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+    public JwtConfig() {}
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getTokenPrefix() {
+        return tokenPrefix;
+    }
+
+    public void setTokenPrefix(String tokenPrefix) {
+        this.tokenPrefix = tokenPrefix;
+    }
+
+    public Integer getTokenExpirationAfterDays() {
+        return tokenExpirationAfterDays;
+    }
+
+    public void setTokenExpirationAfterDays(Integer tokenExpirationAfterDays) {
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+    }
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+    /*
+    public static SecretKey convertStringToSecretKeyto(String encodedKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+        SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+        return originalKey;
+    }*/
+}
--- toiletprojectbackend/src/main/java/com/jasb/toiletproject/rest/ToiletRestController.java
+++ toiletprojectbackend/src/main/java/com/jasb/toiletproject/rest/ToiletRestController.java
@@ -7,37 +7,33 @@ import com.jasb.toiletproject.domain.Toilet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("api/v1/toilets")
 public class ToiletRestController {
-    /*
-    @RequestMapping(value = "/onetoilet", method = RequestMethod.GET)
-    public String oneToilet() {
-        //List<Toilet> allToilets = data.findAll();
-        //List<Toilet> allToilets = List.of(new Toilet(1,2.1,2.1));
-        Toilet toilet = new Toilet(1,2.2,2.1);
-        return toilet.toString();
-    }
-    */
 
     @Autowired
     ToiletRepository data;
 
-    @RequestMapping(value = "/toilets", method = RequestMethod.GET)
+    @GetMapping()
+    @PreAuthorize("hasAnyRole('ROLE_APPUSER', 'ROLE_ADMIN')")
     public ToiletList allToilets() {
         return new ToiletList(data.findAll());
     }
 
-    @RequestMapping(value = "/toilets", method = RequestMethod.POST)
+    @PostMapping()
+    @PreAuthorize("hasAnyRole('ROLE_APPUSER', 'ROLE_ADMIN')")
     public ResponseEntity addToilet(@RequestBody Toilet t) {
         data.save(t);
         return new ResponseEntity<Toilet>(t, HttpStatus.CREATED);
     }
-    @RequestMapping(value ="/toilets/{id}",method = RequestMethod.GET)
+    @GetMapping(path ="{id}")
+    @PreAuthorize("hasAnyRole('ROLE_APPUSER', 'ROLE_ADMIN')")
     public Optional<Toilet> getToiletById(@PathVariable("id") long id) {
         return data.findById(id);
     }
