6573b4a692d96a96e8252d478f3f54049783dfaa
adrianbook <adrianbook@gmail.com>
2021-11-25 13:33:46 +0100

    added structure to toiletapp

---
+++ toiletprojectfrontend/toilettapp/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MapPage from "./routes/MapPage";
+import Login from "./routes/Login";
+import CreateUser from "./routes/CreateUser";
+
+const App = () => {
+
+    return (
+        <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MapPage/>} />
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/createuser" element={<CreateUser/>}/>
+    </Routes>
+  </BrowserRouter>
+        )}
+
+export default App;
--- toiletprojectfrontend/toilettapp/src/index.js
+++ toiletprojectfrontend/toilettapp/src/index.js
@@ -1,16 +1,9 @@
 import { render } from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MapPage from "./routes/MapPage";
-import Login from "./routes/Login";
-import CreateUser from "./routes/CreateUser";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MapPage/>} />
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/createuser" element={<CreateUser/>}/>
-    </Routes>
-  </BrowserRouter>
+  <>
+    <App />
+  </>
   , rootElement);
