dbbcd4387a9ef921c933d2b5918c891735afdc20
jensbankel <jenban1204@skola.goteborg.se>
2022-01-25 16:39:01 +0100

    added more tests

--- toiletprojectbackend/src/test/java/com/jasb/toiletproject/ToiletprojectApplicationTests.java
+++ toiletprojectbackend/src/test/java/com/jasb/toiletproject/ToiletprojectApplicationTests.java
@@ -38,6 +38,8 @@ import org.springframework.security.test.context.support.WithSecurityContext;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -54,10 +56,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@Transactional
 class ToiletprojectApplicationTests {
 	private final static String TEST_USER_ID = "user-id-123";
 	private final static long TOILET_ID = 1;
@@ -67,6 +69,9 @@ class ToiletprojectApplicationTests {
 	private final static String NOTES = "Important comment";
 	private final static int RATING = 3;
 	private final static double AVG_RATING = 3.0;
+	private final static String LAT_LONG_JSON = "{\"latitude\":57.706,\"longitude\":11.937}";
+	private final static Toilet TOILET = new Toilet(1L, LONGITUDE, LATITUDE, AVG_RATING_PLACEHOLDER);
+	private final static Toilet TOILET_1 = new Toilet(2L, 57.000, 11.900, AVG_RATING_PLACEHOLDER);
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -77,11 +82,11 @@ class ToiletprojectApplicationTests {
 	@Autowired
 	private ToiletRepository toiletRepository;
 
-	/*@Autowired
+	@Autowired
 	private ToiletUserService toiletUserService;
 
 	@Autowired
-	private RatingService ratingService;*/
+	private RatingService ratingService;
 
 	@MockBean
 	private ToiletUserFetcher toiletUserFetcher;
@@ -89,97 +94,86 @@ class ToiletprojectApplicationTests {
 	@Autowired
 	private RatingRepository ratingRepository;
 
-	/*@Retention(RetentionPolicy.RUNTIME)
-	@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
-	public @interface WithMockCustomUser {
-		Role role1 = new Role(1, "ROLE_APPUSER");
 
-		long id() default 1;
-		String username() default "username";
-		String name() default "name";
-		String password() default "password";
-		String email() default "name@mail.com";
-		boolean isBlocked() default false;
-
+	@Test
+	void addToiletTest() throws Exception {
+		mockMvc.perform(post("/api/v1/toilets/create")
+						.with(user("usr").roles("SUPER_ADMIN"))
+						.contentType("application/json")
+						.content(LAT_LONG_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().string("{\"longitude\":11.937,\"latitude\":57.706,\"avgRating\":0.0,\"id\":2}"));
+		/*Optional<Toilet> toilet = toiletRepository.findById(1L);
+		Assertions.assertTrue(toilet.isPresent());
+		Assertions.assertEquals(1, toilet.get().getId());
+		Assertions.assertEquals(LONGITUDE, toilet.get().getLongitude());*/
 	}
-	static class WithMockCustomUserSecurityContextFactory
-			implements WithSecurityContextFactory<WithMockCustomUser> {
-		@Override
-		public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
-			SecurityContext context = SecurityContextHolder.createEmptyContext();
 
-			CustomUserDetails principal =
-					new CustomUserDetails(customUser.id(), customUser.name(),
-							customUser.username(), customUser.password(),
-							customUser.email(), customUser.isBlocked());
-			Authentication auth =
-					new UsernamePasswordAuthenticationToken(principal,
-							"password", principal.loadUserByUsername(
-									"username").getAuthorities());
-			context.setAuthentication(auth);
-			return context;
-		}
-		class CustomUserDetails implements UserDetailsService {
-
-			public CustomUserDetails(long id, String name, String username,
-									 String password, String email,
-									 Boolean isBlocked, Collection<Role> roles) {
-			}
-
-			public CustomUserDetails(long id, String name, String username, String password, String email, boolean blocked) {
-			}
-
-			@Override
-			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				Role role1 = new Role(1, "ROLE_APPUSER");
-				Collection<Role> roles = new ArrayList<>();
-				roles.add(role1);
-				ToiletUser toiletUser = new ToiletUser(1, "name", "username",
-						"password", "name@mail.com", false, roles);
-				Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-				toiletUser.getRoles().forEach(role -> {
-					authorities.add(new SimpleGrantedAuthority(role.getName()));
-				});
-
-
-				return new org.springframework.security.core.userdetails.User(
-						toiletUser.getUsername(),
-						toiletUser.getPassword(),
-						authorities
-				);
-			}
-		}
+	@Test
+	void getToiletByIdTest() throws Exception {
+		toiletRepository.save(TOILET);
+		mockMvc.perform(get("/api/v1/toilets/1")
+						.with(user("usr"))
+						.contentType("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(
+						"{\"longitude\":11.937,\"latitude\":57.706,\"avgRating\":0.0,\"id\":1}"));
 	}
-	*/
 
-	@WithMockUser
-	@Test/*(expected = ToiletUserNotFoundException.class)*/
-	void addToiletTest() throws Exception, ToiletUserNotFoundException {
-		/*given(ToiletUserFetcher.fetchToiletUserByContext()).willReturn(new ToiletUser(
+	@Test
+	void getAllToiletsTest() throws Exception {
+		toiletRepository.save(TOILET);
+		toiletRepository.save(TOILET_1);
+		mockMvc.perform(get("/api/v1/toilets/getalltoilets")
+				.with(user("usr").roles("SUPER_ADMIN"))
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().string
+						("{\"toilets\":[{\"longitude\":11.937,\"latitude\":57.706,\"avgRating\":0.0,\"id\":3},{\"longitude\":57.0,\"latitude\":11.9,\"avgRating\":0.0,\"id\":4}]}"));
+	}
+	
+	@Test
+	void getAllRatingsForToiletTest() throws Exception{
+		// TODO: 2022-01-25 try it! 
+	}
+
+	@Test
+	void setRatingForToiletTest() throws Exception{
+		// TODO: 2022-01-25 userdetails...
+	}
+	
+	@Test
+	void reportToiletTest() throws Exception{
+		// TODO: 2022-01-25 userdetails... 
+	}
+
+/*	@WithMockUser
+	@Test
+	void addSOAToiletTest() throws Exception, ToiletUserNotFoundException {
+		*//*given(ToiletUserFetcher.fetchToiletUserByContext()).willReturn(new ToiletUser(
 				1, "name", "username", "password",
-				"name@mail.com", false, roles));*/
-		/*Mockito.when(toiletUserFetcher.fetchToiletUserByContext()).thenReturn(new ToiletUser(
+				"name@mail.com", false, roles));*//*
+		*//*Mockito.when(toiletUserFetcher.fetchToiletUserByContext()).thenReturn(new ToiletUser(
 				1, "name", "username", "password",
-				"name@mail.com", false, roles));*/
-		/*Mockito.when(ToiletUserFetcher.fetchToiletUserByContext()).thenAnswer(I -> new ToiletUser(
+				"name@mail.com", false, roles));*//*
+		*//*Mockito.when(ToiletUserFetcher.fetchToiletUserByContext()).thenAnswer(I -> new ToiletUser(
 				1, "name", "username", "password",
-				"name@mail.com", false, roles));*/
-		/*JSONObject jsonObject = new JSONObject("{\"longitude\": 11.937," +
+				"name@mail.com", false, roles));*//*
+		*//*JSONObject jsonObject = new JSONObject("{\"longitude\": 11.937," +
 				"\"latitude\": 57.706}");
 		Role role = new Role(1, "ROLE_APPUSER");
 		Collection<Role> roles = new ArrayList<>();
 		roles.add(role);
 		ToiletUser toiletUser = new ToiletUser(1, "testuser", "testuser",
-				"testuser", "testuser", false, roles);*/
+				"testuser", "testuser", false, roles);*//*
 
-		/*mockMvc.perform(post("/api/user/save")
+		*//*mockMvc.perform(post("/api/user/save")
 						.with(user("testuser"))
 						.with(csrf())
 				.contentType("application/json")
 				.content(objectMapper.writeValueAsString(toiletUser)))
-				.andExpect(status().isCreated());*/
-
-
+				.andExpect(status().isCreated());*//*
 
 		try(MockedStatic<ToiletUserFetcher> theMock =
 					Mockito.mockStatic(ToiletUserFetcher.class)) {
@@ -204,8 +198,8 @@ class ToiletprojectApplicationTests {
 			//Assertions.assertEquals(ToiletUserFetcher.fetchToiletUserByUsername("username"), toiletUser);
 
 			mockMvc.perform(post("/api/v1/toilets/create")
-							/*.with(user("username"))
-							.with(csrf())*/
+							*//*.with(user("username"))
+							.with(csrf())*//*
 							.contentType("application/json")
 							.content(objectMapper.writeValueAsString(toilet)))
 					.andExpect(status().isCreated());
@@ -214,8 +208,8 @@ class ToiletprojectApplicationTests {
 			Assertions.assertEquals(LONGITUDE, toiletA.get().getLongitude());
 
 			mockMvc.perform(get("/api/v1/toilets/1")
-							/*.with(user("username"))
-							.with(csrf())*/
+							*//*.with(user("username"))
+							.with(csrf())*//*
 							.contentType("application/json"))
 					.andExpect(status().isOk())
 					.andExpect(content().string(
@@ -236,21 +230,21 @@ class ToiletprojectApplicationTests {
 			Assertions.assertEquals(LATITUDE, toilets.get(0).getLatitude());
 
 			mockMvc.perform(put("/api/v1/toilets/rate")
-							/*.with(user("username"))
-							.with(csrf())*/
+							*//*.with(user("username"))
+							.with(csrf())*//*
 							.contentType("application/json")
 							.content(objectMapper.writeValueAsString(rating)))
-					/*.andExpect(status().isCreated())*/;
-			/*Assertions.assertEquals(optionalToilet.get().getAvgRating(), AVG_RATING);*/
+					.andExpect(status().isCreated());
+			*//*Assertions.assertEquals(optionalToilet.get().getAvgRating(), AVG_RATING);*//*
 
 			mockMvc.perform(get("/api/v1/toilets/1/rating")
-							/*.with(user("username"))
-							.with(csrf())*/
+							*//*.with(user("username"))
+							.with(csrf())*//*
 							.contentType(MediaType.APPLICATION_JSON))
 					.andExpect(status().isBadRequest()); //den är ju tom...
 			List<Rating> rating1 = ratingRepository.findAll();
 			Assertions.assertTrue(rating1.isEmpty()); //tom tom tom
-			/*Assertions.assertEquals(3.0, ratingRepository.findAvgRating(1));*/
+			*//*Assertions.assertEquals(3.0, ratingRepository.findAvgRating(1));*//*
 		}
-	}
+	}*/
 }
--- toiletuserservice/src/test/java/com/jasb/toiletuserservice/ToiletuserserviceApplicationTests.java
+++ toiletuserservice/src/test/java/com/jasb/toiletuserservice/ToiletuserserviceApplicationTests.java
@@ -20,6 +20,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -36,34 +37,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@Transactional
 class ToiletuserserviceApplicationTests {
 
-	@Autowired
-	private ObjectMapper objectMapper;
+	/*@Autowired
+	private ObjectMapper objectMapper;*/
+	/*@Autowired
+	private RoleRepo roleRepo;*/
+
 	@Autowired
 	private MockMvc mockMvc;
-	@Autowired
-	private RoleRepo roleRepo;
 	@Autowired
 	private ToiletUserServiceImpl userService;
 
 	private final static String TOILET_USER_1 = "{\"email\":\"john.doe@mail.com\",\"name\":\"John Doe\",\"password\":\"secret\",\"username\":\"jd\"}";
 	private final static String TOILET_USER_2 = "{\"email\":\"joanna.doe@mail.com\",\"name\":\"Joanna Doe\",\"password\":\"secret\",\"username\":\"jo\"}";
+	private final static String TOILET_USER_3 = "{\"email\":\"johnny.doe@mail.com\",\"name\":\"Johnny Doe\",\"password\":\"secret\",\"username\":\"jy\"}";
 	private final static String ROLE = "{\"name\":\"ROLE_APPUSER\"}";
+	private final static String ROLE_TO_USER_FORM = "{\"username\":\"jd\",\"rolename\":\"ROLE_APPUSER\"}";
 
 	@Test
-	void saveToiletUserTest() throws Exception {
+	void saveRoleTest() throws Exception {
 		mockMvc.perform(post("/api/role/save")
 						.with(user("superduperadmin").roles("SUPER_ADMIN"))
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(ROLE))
-				.andExpect(status().isCreated());
+				.andExpect(status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("ROLE_APPUSER"));
+	}
+
+	@Test
+	void saveToiletUserTest() throws Exception {
+		/*mockMvc.perform(post("/api/role/save")
+						.with(user("superduperadmin").roles("SUPER_ADMIN"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(ROLE))
+				.andExpect(status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("ROLE_APPUSER"));*/
 
 		mockMvc.perform(post("/api/user/save")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(TOILET_USER_1))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(TOILET_USER_1))
 				.andExpect(status().isCreated())
 				.andExpect(content().string("User created"));
+
 		Assertions.assertNotNull(userService.getToiletUser("jd"));
 		Assertions.assertEquals("john.doe@mail.com",
 				userService.getToiletUser("jd").getEmail());
@@ -76,30 +93,16 @@ class ToiletuserserviceApplicationTests {
 	}
 
 	@Test
-	void getToiletUsersTest() throws Exception {
-		mockMvc.perform(post("/api/user/save")
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(TOILET_USER_1))
-				.andExpect(status().isCreated());
-		mockMvc.perform(post("/api/user/save")
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(TOILET_USER_2))
-				.andExpect(status().isCreated());
-
-		mockMvc.perform(get("/api/users")
-						.with(user("user").roles("SUPER_ADMIN"))
-						.contentType(MediaType.APPLICATION_JSON))
-						.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("jd"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[1].username").value("jo"));
-	}
-
-	@Test
 	void getToiletUserTest() throws Exception {
 		mockMvc.perform(post("/api/user/save")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(TOILET_USER_1))
+				.andExpect(status().isCreated())
+				.andExpect(content().string("User created"));
+
+		mockMvc.perform(post("/api/user/save")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(TOILET_USER_2))
 				.andExpect(status().isCreated());
 
 		mockMvc.perform(get("/api/user/jd")
@@ -108,5 +111,70 @@ class ToiletuserserviceApplicationTests {
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.email").value(
 						"john.doe@mail.com"));
+
+		mockMvc.perform(get("/api/user/jo")
+						.with(user("user").roles("SUPER_ADMIN"))
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.email").value(
+						"joanna.doe@mail.com"));
+
+
+	}
+
+	@Test
+	void getToiletAllUsersTest() throws Exception {
+		mockMvc.perform(post("/api/user/save")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(TOILET_USER_1))
+				.andExpect(status().isCreated())
+				.andExpect(content().string("User created"));
+
+		mockMvc.perform(post("/api/user/save")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(TOILET_USER_2))
+				.andExpect(status().isCreated());
+
+		mockMvc.perform(post("/api/user/save")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(TOILET_USER_3))
+				.andExpect(status().isCreated());
+
+
+		mockMvc.perform(get("/api/users")
+						.with(user("user").roles("SUPER_ADMIN"))
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("jd"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].username").value("jo"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[2].username").value("jy"));
+		/*.andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("jy"))*/
+		;
+	}
+
+	@Test
+	void addRoleToUserTest() throws Exception {
+		mockMvc.perform(post("/api/user/save")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(TOILET_USER_1))
+				.andExpect(status().isCreated())
+				.andExpect(content().string("User created"));
+
+		mockMvc.perform(post("/api/role/addtouser")
+						.with(user("superduperadmin").roles("SUPER_ADMIN"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(ROLE_TO_USER_FORM))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void getTokenVerification() throws Exception {
+		// TODO: 2022-01-25
+	}
+
+	@Test
+	void blockUserTest() throws Exception {
+		// TODO: 2022-01-25
 	}
 }
