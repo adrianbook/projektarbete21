326788d17d49e0194edf4d7c841e3f25dda2b64e
Staffan <staffan.godman@gmail.com>
2022-01-27 11:12:06 +0100

    Added scripts for seeding table role. Updated README and docker compose with new volume. Cleaned up Proximity and ProximityTest

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # projektarbete21
 Projektarbete Java 20 2/11-21 -- 28/1-22
 
+To setup database run following in command line: 
+docker exec -it db-dev bash scripts/setup.bash
--- toiletprojectbackend/src/test/java/com/jasb/toiletproject/util/ProximityTest.java
+++ toiletprojectbackend/src/test/java/com/jasb/toiletproject/util/ProximityTest.java
@@ -11,34 +11,34 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProximityTest {
-/*    private static final Double longitude = 10.00000;
+    private static final Double longitude = 10.00000;
     private static final Double latitude = 12.00000;
     private static final Double minimumDistance = 0.00009;
     private static final List<Toilet> existingToilets = new ArrayList<>();
 
     @BeforeAll
     public static void addExistingToilets(){
-        existingToilets.add(new Toilet(1, longitude, latitude, 5));
+        existingToilets.add(new Toilet(1, longitude, latitude, false, false, false, false, false, false, 5));
     }
 
     @Test
     public void testToiletAtSameLatLng() {
-        Toilet toiletAtSameCoordinates = new Toilet(2, longitude, latitude, 1);
-        assertTrue(Proximity.toClose(toiletAtSameCoordinates, existingToilets));
+        Toilet toiletAtSameCoordinates = new Toilet(2, longitude, latitude, false, false, false, false, false, false, 1);
+        assertTrue(Proximity.tooClose(toiletAtSameCoordinates, existingToilets));
     }
 
     @Test
     public void testToiletTooClose() {
-        Toilet toiletTooClose = new Toilet(3, longitude+0.00001, latitude+0.00001, 1);
-        assertTrue(Proximity.toClose(toiletTooClose, existingToilets));
+        Toilet toiletTooClose = new Toilet(3, longitude+0.00001, latitude+0.00001, false, false, false, false, false, false, 1);
+        assertTrue(Proximity.tooClose(toiletTooClose, existingToilets));
     }
 
     @Test
     public void testToiletNotTooClose() {
-        Toilet toiletLongitudeNotTooClose = new Toilet(4, longitude+minimumDistance, latitude, 1);
-        Toilet toiletLatitudeNotTooClose = new Toilet(4, longitude, latitude+minimumDistance, 1);
-        assertFalse(Proximity.toClose(toiletLongitudeNotTooClose, existingToilets));
-        assertFalse(Proximity.toClose(toiletLatitudeNotTooClose, existingToilets));
-    }*/
+        Toilet toiletLongitudeNotTooClose = new Toilet(4, longitude+minimumDistance, latitude, false, false, false, false, false, false, 1);
+        Toilet toiletLatitudeNotTooClose = new Toilet(4, longitude, latitude+minimumDistance, false, false, false, false, false, false, 1);
+        assertFalse(Proximity.tooClose(toiletLongitudeNotTooClose, existingToilets));
+        assertFalse(Proximity.tooClose(toiletLatitudeNotTooClose, existingToilets));
+    }
 
 }
